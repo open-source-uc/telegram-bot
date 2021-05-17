88a938f0c549995a3ac2515f56959dbe7516e0cf
Dyotson <maxmilitzer@gmail.com>
2021-05-17 12:11:22 -0400

    Primera funcion lista, ahora el bot responde cuando la persona se une al grupo

--- botprogram.py
+++ botprogram.py
@@ -1,8 +1,39 @@
-import telegram
+from telegram import Update, Chat, ChatMember, ParseMode, ChatMemberUpdated, Bot
+from typing import Tuple, Optional
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackContext,
+    ChatMemberHandler,
+)
 import basic
+import logging
 
 my_token = '1878681564:AAFooinZW8l7aVNAzFypkVxJsEgl72TdPrg'
-bot = telegram.Bot(token=my_token)
 
-mensaje = bot.get_me()
-print(mensaje)
+bot = Bot(token=my_token)
+
+while True:
+    updater = Updater(my_token)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Keep track of which chats the bot is in
+    dispatcher.add_handler(ChatMemberHandler(basic.track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
+    dispatcher.add_handler(CommandHandler("show_chats", basic.show_chats))
+
+    # Handle members joining/leaving chats.
+    dispatcher.add_handler(ChatMemberHandler(basic.greet_chat_members, ChatMemberHandler.CHAT_MEMBER))
+
+    # Start the Bot
+    # We pass 'allowed_updates' to *only* handle updates with '(my_)chat_member' or 'message'
+    # If you want to handle *all* updates, pass Update.ALL_TYPES
+    updater.start_polling(
+        allowed_updates=[Update.MESSAGE, Update.CHAT_MEMBER, Update.MY_CHAT_MEMBER]
+    )
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
